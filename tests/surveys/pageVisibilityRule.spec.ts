0ccea5c62f32afe7268f0856c50171d1887fb606
Stevan Freeborn <sfreeborn7@gmail.com>
2022-10-14 16:48:38 -0500

    begin stubbing out tests for page visibility rules.

---
+++ tests/surveys/pageVisibilityRule.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('page visibility rule', () => {
+    test('each survey page has a tab titled page visibility', async ({ page }) => {
+
+    });
+
+    test('each page visibility tab contains a section titled visibility rule', async({ page }) => {
+
+    });
+
+    test('each visibility rule contains a help icon with correct label text', async ({ page }) => {
+
+    });
+
+    test('each visibility rule section contains a checkbox with correct label text', async ({ page }) => {
+
+    });
+
+    test('when the visibility rule checkbox is checked the page visibility rule control is displayed', async ({ page }) => {
+
+    });
+
+    test('when the visibility rule checkbox is unchecked the page visibility rule control is hidden', async ({ page }) => {
+
+    });
+
+    test('the page visibility rule control has correct parts', async ({ page }) => {
+
+    });
+
+    test('the page visibility rule control supports simple and advanced mode', async ({ page }) => {
+
+    });
+});
