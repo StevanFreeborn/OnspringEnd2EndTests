e3f4840dd1180551d76c1b643cbc00eee40e582e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-01 15:02:06 -0500

    fix: duplicate name

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,10 +1,10 @@
 import { expect, test } from '../fixtures';
 
 test.describe('Test suite', () => {
-  test('Test', () => {
+  test('Test 1', () => {
     expect(1).toBe(1);
   });
-  test('Test', () => {
+  test('Test 2', () => {
     expect(2).toBe(1);
   });
 });
