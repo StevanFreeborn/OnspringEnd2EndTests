0c2be5dbcf844fe2969f520cfc3f5921db6eec17
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-01 15:01:36 -0500

    tests: add test to check if workflow works

---
+++ tests/test.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from '../fixtures';
+
+test.describe('Test suite', () => {
+  test('Test', () => {
+    expect(1).toBe(1);
+  });
+  test('Test', () => {
+    expect(2).toBe(1);
+  });
+});
