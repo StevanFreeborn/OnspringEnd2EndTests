dd25238133835a8bb30bd763da24bb144577ffe0
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-05 00:11:51 -0500

    chore: stub out tests

---
+++ tests/fields/dateField.spec.ts
@@ -0,0 +1,114 @@
+import { expect, fieldTest as test } from '../../fixtures';
+import { AnnotationType } from '../annotations';
+
+test.describe('date/time field', () => {
+  test('Add a Date/Time Field to an app from the Fields & Objects report', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-56',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+
+  test('Create a copy of a Date/Time Field on an app from the Fields & Objects report', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-57',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+
+  test('Add a Date/Time Field to an app from a layout', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-58',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+
+  test('Create a copy of a Date/Time Field on an app from a layout', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-59',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+
+  test("Add a Date/Time Field to an app's layout", async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-64',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+
+  test("Remove a Date/Time Field from an app's layout", async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-69',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+
+  test('Delete a Date/Time Field from an app', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-70',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+
+  test('Update the configuration of a Date/Time Field on an app', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-73',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+
+  test('Make a Date/Time Field private by role to prevent access', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-98',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+
+  test('Make a Date/Time Field private by role to give access', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-812',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+
+  test('Make a Date/Time Field public', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-102',
+    });
+
+    // TODO: Implement test
+    expect(false).toBe(true);
+  });
+});
