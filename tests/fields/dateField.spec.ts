c63c244efdfcdf3e1b778b96ccdd2851574d4efd
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-06 11:11:57 -0600

    tests: add test for test-98

--- tests/fields/dateField.spec.ts
+++ tests/fields/dateField.spec.ts
@@ -1,7 +1,10 @@
 import { FakeDataFactory } from '../../factories/fakeDataFactory';
 import { Locator, expect, fieldTest as test } from '../../fixtures';
 import { DateField } from '../../models/dateField';
+import { LayoutItemPermission } from '../../models/layoutItem';
 import { AddContentPage } from '../../pageObjectModels/content/addContentPage';
+import { EditContentPage } from '../../pageObjectModels/content/editContentPage';
+import { ViewContentPage } from '../../pageObjectModels/content/viewContentPage';
 import { AnnotationType } from '../annotations';
 
 test.describe('date/time field', () => {
@@ -349,14 +352,72 @@ test.describe('date/time field', () => {
     });
   });
 
-  test('Make a Date/Time Field private by role to prevent access', async () => {
+  test('Make a Date/Time Field private by role to prevent access', async ({
+    sysAdminPage,
+    appAdminPage,
+    app,
+    role,
+    testUserPage,
+  }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-98',
     });
 
-    // TODO: Implement test
-    expect(false).toBe(true);
+    const field = new DateField({
+      name: FakeDataFactory.createFakeFieldName(),
+      permissions: [new LayoutItemPermission({ roleName: role.name, read: false, update: false })],
+    });
+    const tabName = 'Tab 2';
+    const sectionName = 'Section 1';
+    const addContentPage = new AddContentPage(sysAdminPage);
+    const editContentPage = new EditContentPage(sysAdminPage);
+    const viewContentPage = new ViewContentPage(testUserPage);
+    let recordId: number;
+
+    await test.step('Add the date/time field', async () => {
+      await appAdminPage.goto(app.id);
+      await appAdminPage.layoutTabButton.click();
+      await appAdminPage.layoutTab.addLayoutItemFromFieldsAndObjectsGrid(field);
+      await appAdminPage.layoutTab.openLayout();
+      await appAdminPage.layoutTab.layoutDesignerModal.dragFieldOnToLayout({
+        tabName: 'Tab 2',
+        sectionName: 'Section 1',
+        sectionColumn: 0,
+        sectionRow: 0,
+        fieldName: field.name,
+      });
+      await appAdminPage.layoutTab.layoutDesignerModal.saveAndCloseLayout();
+    });
+
+    await test.step('Create a record with a value in the date/time field as system admin', async () => {
+      await addContentPage.goto(app.id);
+      const dateTimePicker = await addContentPage.getField({
+        tabName: tabName,
+        sectionName: sectionName,
+        fieldName: field.name,
+        fieldType: 'Date/Time',
+      });
+      await dateTimePicker.selectDateUsingCalendar(2024, 'January', 1);
+      await addContentPage.saveRecordButton.click();
+      await addContentPage.page.waitForURL(editContentPage.pathRegex);
+      await editContentPage.page.waitForLoadState();
+      recordId = editContentPage.getRecordIdFromUrl();
+    });
+
+    await test.step('Navigate to created record as test user who does not have access to the field by their role', async () => {
+      await viewContentPage.goto(app.id, recordId);
+    });
+
+    await test.step('Verify the field is not visible', async () => {
+      const contentField = await viewContentPage.getField({
+        tabName: tabName,
+        sectionName: sectionName,
+        fieldName: field.name,
+        fieldType: 'Date/Time',
+      });
+      await expect(contentField).toBeHidden();
+    });
   });
 
   test('Make a Date/Time Field private by role to give access', async () => {
