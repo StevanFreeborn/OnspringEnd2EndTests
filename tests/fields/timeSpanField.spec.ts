1e08a2e0dce72f3c3c3028d12a587bf14466ae48
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-07 15:02:11 -0600

    tests: add test for test-131

--- tests/fields/timeSpanField.spec.ts
+++ tests/fields/timeSpanField.spec.ts
@@ -1,5 +1,5 @@
 import { FakeDataFactory } from '../../factories/fakeDataFactory';
-import { expect, fieldTest as test } from '../../fixtures';
+import { Locator, expect, fieldTest as test } from '../../fixtures';
 import { TimeSpanField } from '../../models/timeSpanField';
 import { AddContentPage } from '../../pageObjectModels/content/addContentPage';
 import { AnnotationType } from '../annotations';
@@ -232,14 +232,58 @@ test.describe('time span field', async () => {
     });
   });
 
-  test("Remove a Time Span Field from an app's layout", async () => {
+  test("Remove a Time Span Field from an app's layout", async ({ sysAdminPage, appAdminPage, app }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-131',
     });
 
-    // TODO: Implement this test
-    expect(false).toBe(true);
+    const field = new TimeSpanField({ name: FakeDataFactory.createFakeFieldName() });
+    const tabName = 'Tab 2';
+    const sectionName = 'Section 1';
+    let fieldInBank: Locator;
+    let fieldLayoutDropzone: Locator;
+
+    await test.step('Add the time span field that will be removed from layout', async () => {
+      await appAdminPage.layoutTab.openLayout();
+      await appAdminPage.layoutTab.addLayoutItemFromLayoutDesigner(field);
+      const { field: fieldFromBank, dropzone } = await appAdminPage.layoutTab.layoutDesignerModal.dragFieldOnToLayout({
+        tabName: tabName,
+        sectionName: sectionName,
+        sectionColumn: 0,
+        sectionRow: 0,
+        fieldName: field.name,
+      });
+
+      fieldLayoutDropzone = dropzone;
+      fieldInBank = fieldFromBank;
+
+      await appAdminPage.layoutTab.layoutDesignerModal.saveAndCloseLayout();
+    });
+
+    await test.step('Remove the time span field from the layout', async () => {
+      await appAdminPage.layoutTab.openLayout();
+      await fieldLayoutDropzone.hover();
+      await fieldLayoutDropzone.getByTitle('Remove Field from Layout').click();
+
+      await expect(fieldInBank).not.toHaveClass(/ui-draggable-disabled/);
+      await expect(fieldLayoutDropzone).not.toHaveText(new RegExp(field.name));
+
+      await appAdminPage.layoutTab.layoutDesignerModal.saveAndCloseLayout();
+    });
+
+    await test.step('Verify the field was removed from the layout', async () => {
+      const addContentPage = new AddContentPage(sysAdminPage);
+      await addContentPage.goto(app.id);
+      const contentField = await addContentPage.getField({
+        tabName: tabName,
+        sectionName: sectionName,
+        fieldName: field.name,
+        fieldType: 'Time Span',
+      });
+
+      await expect(contentField.control).toBeHidden();
+    });
   });
 
   test('Update the configuration of a Time Span Field on an app', async () => {
