4cdd43ab0a7d1582e27d1f87e102e3ebe79085fa
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-08 16:14:44 -0600

    tests: add test for test-168

--- tests/fields/listFormulaField.spec.ts
+++ tests/fields/listFormulaField.spec.ts
@@ -118,14 +118,41 @@ test.describe('list formula field', () => {
     });
   });
 
-  test('Add a List Formula Field to an app from a layout', async () => {
+  test('Add a List Formula Field to an app from a layout', async ({ appAdminPage }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-168',
     });
 
-    // TODO: implement test
-    expect(false).toBe(true);
+    const listValues = [new ListValue({ value: 'No' }), new ListValue({ value: 'Yes' })];
+    const field = new ListFormulaField({
+      name: FakeDataFactory.createFakeFieldName(),
+      values: listValues,
+      formula: `return [:${listValues[0].value}];`,
+    });
+
+    await test.step('Open layout designer for default layout', async () => {
+      await appAdminPage.layoutTab.openLayout();
+    });
+
+    await test.step('Add the list formula field', async () => {
+      await appAdminPage.layoutTab.addLayoutItemFromLayoutDesigner(field);
+    });
+
+    await test.step('Verify the field was added', async () => {
+      const fieldInBank = appAdminPage.layoutTab.layoutDesignerModal.layoutItemsSection.fieldsTab.getFieldFromBank(
+        field.name
+      );
+
+      await expect(fieldInBank).toBeVisible();
+      await expect(fieldInBank).not.toHaveClass(/ui-draggable-disabled/);
+
+      await appAdminPage.layoutTab.layoutDesignerModal.closeButton.click();
+
+      const fieldRow = appAdminPage.layoutTab.fieldsAndObjectsGrid.getByRole('row', { name: field.name });
+
+      await expect(fieldRow).toBeVisible();
+    });
   });
 
   test('Create a copy of a List Formula Field on an app from a layout', async () => {
