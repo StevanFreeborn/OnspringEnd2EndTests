f93e433a792cd84e15fec177c7f3ddf47d116254
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-11-09 15:23:25 -0600

    tests: add test for test-88, test-89, and test-852

--- tests/fields/attachmentField.spec.ts
+++ tests/fields/attachmentField.spec.ts
@@ -1,41 +1,108 @@
+import { FakeDataFactory } from '../../factories/fakeDataFactory';
 import { expect, fieldTest as test } from '../../fixtures';
+import { AttachmentField } from '../../models/attachmentField';
 import { AnnotationType } from '../annotations';
 
 test.describe('attachment field', () => {
-  test('Add an Attachment Field to an app from the Fields & Objects report', async () => {
+  test.beforeEach(async ({ appAdminPage, app }) => {
+    await appAdminPage.goto(app.id);
+    await appAdminPage.layoutTabButton.click();
+  });
+
+  test('Add an Attachment Field to an app from the Fields & Objects report', async ({ appAdminPage }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-88',
     });
 
-    // TODO: Implement this test
-    expect(false).toBe(true);
-  });
+    const field = new AttachmentField({ name: FakeDataFactory.createFakeFieldName() });
 
-  test('Create a copy of an Attachment Field on an app from the Fields & Objects report using the row copy button', async () => {
-    test.info().annotations.push({
-      type: AnnotationType.TestId,
-      description: '',
+    await test.step('Add the attachment field', async () => {
+      await appAdminPage.layoutTab.addLayoutItemFromFieldsAndObjectsGrid(field);
     });
 
-    // TODO: Implement this test
-    expect(false).toBe(true);
+    await test.step('Verify the field was added', async () => {
+      const fieldRow = appAdminPage.layoutTab.fieldsAndObjectsGrid.getByRole('row', { name: field.name });
+      await expect(fieldRow).toBeVisible();
+    });
   });
 
-  test('Create a copy of an Attachment Field on an app from the Fields & Objects report using the Add Field button', async () => {
+  test('Create a copy of an Attachment Field on an app from the Fields & Objects report using the row copy button', async ({
+    appAdminPage,
+  }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
-      description: '',
+      description: 'Test-89',
     });
 
-    // TODO: Implement this test
-    expect(false).toBe(true);
+    const field = new AttachmentField({ name: FakeDataFactory.createFakeFieldName() });
+    const copiedFieldName = `${field.name} (1)`;
+
+    await test.step('Add the the attachment field to be copied', async () => {
+      await appAdminPage.layoutTab.addLayoutItemFromFieldsAndObjectsGrid(field);
+    });
+
+    await test.step('Add a copy of the attachment field', async () => {
+      const fieldRow = appAdminPage.layoutTab.fieldsAndObjectsGrid.getByRole('row', { name: field.name });
+      await fieldRow.hover();
+      await fieldRow.getByTitle('Copy').click();
+
+      const addAttachmentFieldModal = appAdminPage.layoutTab.getLayoutItemModal('Attachment');
+      await expect(addAttachmentFieldModal.generalTab.fieldInput).toHaveValue(copiedFieldName);
+      await addAttachmentFieldModal.saveButton.click();
+    });
+
+    await test.step('Verify the field was copied', async () => {
+      await appAdminPage.page.waitForLoadState('networkidle');
+      const copiedFieldRow = appAdminPage.layoutTab.fieldsAndObjectsGrid.getByRole('row', {
+        name: copiedFieldName,
+      });
+      await expect(copiedFieldRow).toBeVisible();
+    });
+  });
+
+  test('Create a copy of an Attachment Field on an app from the Fields & Objects report using the Add Field button', async ({
+    appAdminPage,
+  }) => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-852',
+    });
+
+    const field = new AttachmentField({ name: FakeDataFactory.createFakeFieldName() });
+    const copiedFieldName = `${field.name} (1)`;
+
+    await test.step('Add the attachment field to copy', async () => {
+      await appAdminPage.layoutTab.addLayoutItemFromFieldsAndObjectsGrid(field);
+    });
+
+    await test.step('Add a copy of the attachment field', async () => {
+      await appAdminPage.layoutTab.addFieldButton.click();
+      await appAdminPage.layoutTab.addLayoutItemMenu.selectItem(field.type);
+      await appAdminPage.layoutTab.addLayoutItemDialog.copyFromRadioButton.click();
+      await appAdminPage.layoutTab.addLayoutItemDialog.selectDropdown.click();
+      await appAdminPage.layoutTab.addLayoutItemDialog.getLayoutItemToCopy(field.name).click();
+      await appAdminPage.layoutTab.addLayoutItemDialog.continueButton.click();
+
+      const addAttachmentFieldModal = appAdminPage.layoutTab.getLayoutItemModal('Attachment');
+
+      await expect(addAttachmentFieldModal.generalTab.fieldInput).toHaveValue(copiedFieldName);
+
+      await addAttachmentFieldModal.saveButton.click();
+    });
+
+    await test.step('Verify the field was copied', async () => {
+      const copiedFieldRow = appAdminPage.layoutTab.fieldsAndObjectsGrid.getByRole('row', {
+        name: copiedFieldName,
+      });
+      await expect(copiedFieldRow).toBeVisible();
+    });
   });
 
   test('Add an Attachment Field to an app from a layout', async () => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
-      description: '',
+      description: 'Test-90',
     });
 
     // TODO: Implement this test
@@ -45,7 +112,7 @@ test.describe('attachment field', () => {
   test('Create a copy of an Attachment Field on an app from a layout', async () => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
-      description: '',
+      description: 'Test-91',
     });
 
     // TODO: Implement this test
