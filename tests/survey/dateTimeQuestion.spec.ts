ecef3a96f8abab876312df2cd98976c6e5bf6895
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-01-27 08:29:28 -0600

    chore: stub out tests for date time question

---
+++ tests/survey/dateTimeQuestion.spec.ts
@@ -0,0 +1,74 @@
+import { expect, surveyQuestionTest as test } from '../../fixtures';
+import { AnnotationType } from '../annotations';
+
+test.describe('date/time question', () => {
+  test('Create a date/time question', async ({}) => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-344',
+    });
+
+    // TODO: Implement test
+    expect(true).toBeTruthy();
+  });
+
+  test('Create a copy of a date/time question', async ({}) => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-345',
+    });
+
+    // TODO: Implement test
+    expect(true).toBeTruthy();
+  });
+
+  test('Import a date/time question', async ({}) => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-346',
+    });
+
+    // TODO: Implement test
+    expect(true).toBeTruthy();
+  });
+
+  test('Update a date/time question', async ({}) => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-347',
+    });
+
+    // TODO: Implement test
+    expect(true).toBeTruthy();
+  });
+
+  test('Move a date/time question on a page', async ({}) => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-348',
+    });
+
+    // TODO: Implement test
+    expect(true).toBeTruthy();
+  });
+
+  test('Move a date/time question to another page.', async ({}) => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-349',
+    });
+
+    // TODO: Implement test
+    expect(true).toBeTruthy();
+  });
+
+  test('Delete a date/time question', async ({}) => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-350',
+    });
+
+    // TODO: Implement test
+    expect(true).toBeTruthy();
+  });
+});
