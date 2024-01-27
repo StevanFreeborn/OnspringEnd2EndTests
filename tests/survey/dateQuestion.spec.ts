c5de86d457398f86c7e4e2b830aadb5e181afc56
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-01-27 08:43:54 -0600

    tests: add test for Test-344

--- tests/survey/dateQuestion.spec.ts
+++ tests/survey/dateQuestion.spec.ts
@@ -1,15 +1,42 @@
+import { FakeDataFactory } from '../../factories/fakeDataFactory';
 import { expect, surveyQuestionTest as test } from '../../fixtures';
+import { DateQuestion } from '../../models/dateQuestion';
 import { AnnotationType } from '../annotations';
 
 test.describe('date/time question', () => {
-  test('Create a date/time question', async ({}) => {
+  test('Create a date/time question', async ({ targetSurvey: survey, surveyAdminPage }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-344',
     });
 
-    // TODO: Implement test
-    expect(true).toBeTruthy();
+    const questionId = FakeDataFactory.createFakeQuestionId();
+
+    const dateQuestion = new DateQuestion({
+      questionId: questionId,
+      questionText: questionId,
+    });
+
+    let surveyItemId: string;
+
+    await test.step('Navigate to survey admin page', async () => {
+      await surveyAdminPage.goto(survey.id);
+    });
+
+    await test.step('Open the survey designer', async () => {
+      await surveyAdminPage.designTabButton.click();
+      await surveyAdminPage.designTab.openSurveyDesigner();
+    });
+
+    await test.step('Add a date question', async () => {
+      surveyItemId = await surveyAdminPage.designTab.surveyDesignerModal.addQuestion(dateQuestion);
+    });
+
+    await test.step('Preview the survey and confirm the date question is present', async () => {
+      const previewPage = await surveyAdminPage.designTab.surveyDesignerModal.previewSurvey();
+      const createdQuestion = previewPage.getQuestion(surveyItemId, dateQuestion.questionText);
+      await expect(createdQuestion).toBeVisible();
+    });
   });
 
   test('Create a copy of a date/time question', async ({}) => {
