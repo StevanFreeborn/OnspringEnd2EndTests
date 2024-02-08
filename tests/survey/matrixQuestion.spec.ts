b0da0fea476212588d9b79c43106c1d37e1380b2
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-02-07 23:23:31 -0600

    tests: add test for Test-555

--- tests/survey/matrixQuestion.spec.ts
+++ tests/survey/matrixQuestion.spec.ts
@@ -123,22 +123,102 @@ test.describe('matrix question', () => {
     });
   });
 
-  test('Import a matrix question', async ({}) => {
+  test('Import a matrix question', async ({ sourceSurvey, surveyAdminPage }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-554',
     });
 
-    expect(true).toBe(true);
+    const questionId = FakeDataFactory.createFakeQuestionId();
+
+    const sourceMatrixQuestion = new MatrixQuestion({
+      questionId: questionId,
+      questionText: questionId,
+      rowValues: [new BaseListValue({ value: 'Row 1' }), new BaseListValue({ value: 'Row 2' })],
+      columnValues: [new BaseListValue({ value: 'Column 1' }), new BaseListValue({ value: 'Column 2' })],
+    });
+
+    let questionCreatedViaImport: string;
+
+    await test.step("Navigate to the source survey's admin page", async () => {
+      await surveyAdminPage.goto(sourceSurvey.id);
+    });
+
+    await test.step("Open the source survey's survey designer", async () => {
+      await surveyAdminPage.designTabButton.click();
+      await surveyAdminPage.designTab.openSurveyDesigner();
+    });
+
+    await test.step('Add a matrix question to the source survey', async () => {
+      await surveyAdminPage.designTab.surveyDesignerModal.addQuestion(sourceMatrixQuestion);
+    });
+
+    await test.step("Navigate to the target survey's admin page", async () => {
+      await surveyAdminPage.goto(targetSurvey.id);
+    });
+
+    await test.step("Open the target survey's survey designer", async () => {
+      await surveyAdminPage.designTabButton.click();
+      await surveyAdminPage.designTab.openSurveyDesigner();
+    });
+
+    await test.step('Import the matrix question into the target survey', async () => {
+      questionCreatedViaImport = await surveyAdminPage.designTab.surveyDesignerModal.importQuestion(
+        sourceSurvey.name,
+        sourceMatrixQuestion
+      );
+      surveyItemsToBeDeleted.push({
+        surveyItemId: questionCreatedViaImport,
+      });
+    });
+
+    await test.step('Preview the target survey and confirm the matrix question is present', async () => {
+      const previewPage = await surveyAdminPage.designTab.surveyDesignerModal.previewSurvey();
+      const createdQuestion = previewPage.getQuestion(questionCreatedViaImport, sourceMatrixQuestion.questionText);
+      await expect(createdQuestion).toBeVisible();
+    });
   });
 
-  test('Update a matrix question', async ({}) => {
+  test('Update a matrix question', async ({ surveyAdminPage }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-555',
     });
 
-    expect(true).toBe(true);
+    const questionId = FakeDataFactory.createFakeQuestionId();
+
+    const matrixQuestion = new MatrixQuestion({
+      questionId: questionId,
+      questionText: questionId,
+      rowValues: [new BaseListValue({ value: 'Row 1' }), new BaseListValue({ value: 'Row 2' })],
+      columnValues: [new BaseListValue({ value: 'Column 1' }), new BaseListValue({ value: 'Column 2' })],
+    });
+
+    const updatedQuestion = { ...matrixQuestion, questionText: `${matrixQuestion.questionText} updated` };
+
+    let createdQuestionItemId: string;
+
+    await test.step('Open the survey designer', async () => {
+      await surveyAdminPage.designTabButton.click();
+      await surveyAdminPage.designTab.openSurveyDesigner();
+    });
+
+    await test.step('Create the matrix question to update', async () => {
+      createdQuestionItemId = await surveyAdminPage.designTab.surveyDesignerModal.addQuestion(matrixQuestion);
+      surveyItemsToBeDeleted.push({
+        surveyItemId: createdQuestionItemId,
+      });
+    });
+
+    await test.step('Update the matrix question', async () => {
+      await surveyAdminPage.designTab.surveyDesignerModal.updateQuestion(createdQuestionItemId, updatedQuestion);
+    });
+
+    await test.step('Preview the survey and confirm the updated matrix question is present', async () => {
+      const previewPage = await surveyAdminPage.designTab.surveyDesignerModal.previewSurvey();
+      const updatedQuestionElement = previewPage.getQuestion(createdQuestionItemId, updatedQuestion.questionText);
+      await expect(updatedQuestionElement).toBeVisible();
+    });
   });
 
   test('Move a matrix question on a page', async ({}) => {
