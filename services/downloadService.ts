230a69599ee4e699165dc7b20a01701d1cdc0707
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-02-21 12:54:23 -0600

    fix: use unique name for downloads

--- services/downloadService.ts
+++ services/downloadService.ts
@@ -1,9 +1,11 @@
 import { Download } from '@playwright/test';
 import path from 'path';
+import { FakeDataFactory } from '../factories/fakeDataFactory';
 
 export class DownloadService {
   async saveDownload(download: Download) {
-    const downloadPath = path.join(process.cwd(), 'downloads', download.suggestedFilename());
+    const downloadName = `${FakeDataFactory.createUniqueIdentifier()}-${download.suggestedFilename()}`;
+    const downloadPath = path.join(process.cwd(), 'downloads', downloadName);
     await download.saveAs(downloadPath);
     return downloadPath;
   }
