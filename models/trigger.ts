ec661216f252b429d158673e987b2de175a6e96c
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-04-03 09:50:39 -0500

    feat: add model to represent data needed for a trigger

---
+++ models/trigger.ts
@@ -0,0 +1,38 @@
+import { Rule } from './rule';
+
+type LogicMode = 'Simple Mode' | 'Advanced Mode';
+type LogicOperator = 'AND' | 'OR';
+
+type TriggerObject = {
+  name: string;
+  status: boolean;
+  description?: string;
+  rules?: Rule[];
+  logicMode?: LogicMode;
+  logicOperator?: LogicOperator;
+};
+
+export class Trigger {
+  name: string;
+  status: boolean;
+  description: string;
+  rules: Rule[];
+  logicMode: LogicMode;
+  logicOperator: LogicOperator;
+
+  constructor({
+    name,
+    status,
+    description = '',
+    rules = [],
+    logicMode = 'Simple Mode',
+    logicOperator = 'AND',
+  }: TriggerObject) {
+    this.name = name;
+    this.status = status;
+    this.description = description;
+    this.rules = rules;
+    this.logicMode = logicMode;
+    this.logicOperator = logicOperator;
+  }
+}
