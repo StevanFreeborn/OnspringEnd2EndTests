ad205b37c104fe32830ed74060900b50470746d1
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-05-17 13:11:28 -0500

    fix: make sure to wait for defined records to be fetched before saving

--- componentObjectModels/tabs/definedLibraryContentSelectionSettingsTab.ts
+++ componentObjectModels/tabs/definedLibraryContentSelectionSettingsTab.ts
@@ -3,10 +3,14 @@ import { DefinedLibraryContentDefinition } from '../../models/createMultipleReco
 import { LibraryContentSelectionSettingsTab } from './libraryContentSelectionSettingsTab';
 
 export class DefinedLibraryContentSelectionSettingsTab extends LibraryContentSelectionSettingsTab {
+  private modal: Locator;
   private readonly generateRecordsButton: Locator;
+  private generatePathRegex = /\/Admin\/App\/\d+\/Trigger\/GenerateDefinedRecords/;
+  private getDefinedRecordsPathRegex = /\/Admin\/App\/\d+\/Trigger\/GetDefinedRecordPage/;
 
   constructor(modal: Locator) {
     super(modal);
+    this.modal = modal;
     this.generateRecordsButton = modal.getByRole('button', { name: 'Generate Records' });
   }
 
@@ -18,6 +22,14 @@ export class DefinedLibraryContentSelectionSettingsTab extends LibraryContentSel
       await this.dynamicFilterDualPaneSelector.selectOptions(definition.dynamicFilterFields);
     }
 
+    const page = this.modal.page();
+
+    const generateRecordsResponse = page.waitForResponse(this.generatePathRegex);
+    const getDefinedRecordsResponse = page.waitForResponse(this.getDefinedRecordsPathRegex);
+
     await this.generateRecordsButton.click();
+
+    await generateRecordsResponse;
+    await getDefinedRecordsResponse;
   }
 }
