c9ae42e9acbdde8af88e82b29bf3d46ccc1bde74
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-06-18 17:41:20 -0500

    feat: work on setup for report test

--- tests/report/report.spec.ts
+++ tests/report/report.spec.ts
@@ -1,15 +1,47 @@
+import { FieldType } from '../../componentObjectModels/menus/addFieldTypeMenu';
 import { test as base, expect } from '../../fixtures';
+import { app } from '../../fixtures/app.fixtures';
+import { App } from '../../models/app';
+import { LayoutItem } from '../../models/layoutItem';
+import { ListField } from '../../models/listField';
+import { ListValue } from '../../models/listValue';
+import { AppAdminPage } from '../../pageObjectModels/apps/appAdminPage';
+import { AddContentPage } from '../../pageObjectModels/content/addContentPage';
+import { EditContentPage } from '../../pageObjectModels/content/editContentPage';
 import { AnnotationType } from '../annotations';
 
-type ReportTestFixtures = {};
+type ReportTestFixtures = {
+  sourceApp: App;
+  appAdminPage: AppAdminPage;
+  addContentPage: AddContentPage;
+  editContentPage: EditContentPage;
+};
 
-const test = base.extend<ReportTestFixtures>({});
+const test = base.extend<ReportTestFixtures>({
+  sourceApp: app,
+  appAdminPage: async ({ sysAdminPage }, use) => await use(new AppAdminPage(sysAdminPage)),
+  addContentPage: async ({ sysAdminPage }, use) => await use(new AddContentPage(sysAdminPage)),
+  editContentPage: async ({ sysAdminPage }, use) => await use(new EditContentPage(sysAdminPage)),
+});
 
 test.describe('report', () => {
-  test('Create a report via the "Create Report" button on the report home page', ({}) => {
+  test('Create a report via the "Create Report" button on the report home page', async ({
+    sourceApp,
+    appAdminPage,
+    addContentPage,
+    editContentPage,
+  }) => {
     test.info().annotations.push({
       description: AnnotationType.TestId,
       type: 'Test-594',
+    });
+
+    const fields = getFieldsForApp();
+    let records = buildRecords(fields.groupField, fields.seriesField);
+
+    await test.step('Setup source app with fields and records', async () => {
+      await addFieldsToApp(appAdminPage, sourceApp, Object.values(fields));
+      records = await addRecordsToApp(addContentPage, editContentPage, sourceApp, records);
     });
 
     expect(true).toBe(true);
@@ -285,3 +317,122 @@ test.describe('report', () => {
     expect(true).toBe(true);
   });
 });
+
+function getFieldsForApp() {
+  const groupField = new ListField({
+    name: 'Group',
+    values: [
+      new ListValue({ value: 'Group A' }),
+      new ListValue({ value: 'Group B' }),
+      new ListValue({ value: 'Group C' }),
+    ],
+  });
+
+  const seriesField = new ListField({
+    name: 'Series',
+    values: [
+      new ListValue({ value: 'Series 1' }),
+      new ListValue({ value: 'Series 2' }),
+      new ListValue({ value: 'Series 3' }),
+    ],
+  });
+
+  return { groupField, seriesField };
+}
+
+async function addFieldsToApp(appAdminPage: AppAdminPage, app: App, fields: LayoutItem[]) {
+  await appAdminPage.goto(app.id);
+  await appAdminPage.layoutTabButton.click();
+  await appAdminPage.layoutTab.openLayout();
+
+  for (const [index, field] of fields.entries()) {
+    await appAdminPage.layoutTab.addLayoutItemFromLayoutDesigner(field);
+
+    await appAdminPage.layoutTab.layoutDesignerModal.dragFieldOnToLayout({
+      fieldName: field.name,
+      tabName: 'Tab 2',
+      sectionName: 'Section 1',
+      sectionColumn: 0,
+      sectionRow: index,
+    });
+  }
+
+  await appAdminPage.layoutTab.layoutDesignerModal.saveAndCloseLayout();
+}
+
+type Record = {
+  id: number;
+  fieldValues: { field: string; value: string; tabName: string; sectionName: string; type: FieldType }[];
+};
+
+function buildRecords(groupField: ListField, seriesField: ListField) {
+  const records = [];
+
+  for (const group of groupField.values) {
+    for (const series of seriesField.values) {
+      records.push({
+        id: 0,
+        fieldValues: [
+          {
+            field: groupField.name,
+            value: group.value,
+            tabName: 'Tab 2',
+            sectionName: 'Section 1',
+            type: groupField.type as FieldType,
+          },
+          {
+            field: seriesField.name,
+            value: series.value,
+            tabName: 'Tab 2',
+            sectionName: 'Section 1',
+            type: seriesField.type as FieldType,
+          },
+        ],
+      });
+    }
+  }
+
+  return records;
+}
+
+async function addRecordsToApp(
+  addContentPage: AddContentPage,
+  editContentPage: EditContentPage,
+  app: App,
+  records: Record[]
+) {
+  const createdRecords = [];
+
+  for (const record of records) {
+    await addContentPage.goto(app.id);
+
+    for (const fieldValue of record.fieldValues) {
+      switch (fieldValue.type) {
+        case 'List':
+          {
+            const field = await addContentPage.form.getField({
+              tabName: fieldValue.tabName,
+              sectionName: fieldValue.sectionName,
+              fieldName: fieldValue.field,
+              fieldType: fieldValue.type,
+            });
+
+            await field.click();
+            await field.page().getByRole('option', { name: fieldValue.value }).click();
+          }
+          break;
+        default:
+          throw new Error(`Field type ${fieldValue.type} is not supported.`);
+      }
+    }
+
+    await addContentPage.saveRecordButton.click();
+    await addContentPage.page.waitForURL(editContentPage.pathRegex);
+
+    const createdRecord = { ...record };
+    createdRecord.id = editContentPage.getRecordIdFromUrl();
+    createdRecords.push(createdRecord);
+  }
+
+  return createdRecords;
+}
