93b0d32b8f9508faa2e194e36407a2aa6cd9997f
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-07-01 15:12:18 -0500

    fix: handle auto save dialog with locator handler api

--- componentObjectModels/tabs/surveyDesignTab.ts
+++ componentObjectModels/tabs/surveyDesignTab.ts
@@ -1,31 +1,28 @@
-import { Locator, Page, Response } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 import { SurveyDesignerModal } from '../modals/surveyDesignerModal';
 
 export class SurveyDesignTab {
   private readonly page: Page;
-  private readonly autoSaveAlertPathRegex: RegExp;
   readonly designSurveyLink: Locator;
   readonly surveyDesignerModal: SurveyDesignerModal;
 
   constructor(page: Page) {
     this.page = page;
-    this.autoSaveAlertPathRegex = /\/Admin\/Survey\/\d+\/DesignerAlert/;
     this.designSurveyLink = page.getByRole('link', { name: 'Design Survey' });
     this.surveyDesignerModal = new SurveyDesignerModal(page);
   }
 
   async openSurveyDesigner() {
-    const handleDesignerAlert = async (response: Response) => {
-      if (response.url().match(this.autoSaveAlertPathRegex)) {
-        await this.surveyDesignerModal.autoSaveDialog.dismiss();
-      }
-    };
-
-    this.page.on('response', handleDesignerAlert);
+    const autoSaveDialog = this.surveyDesignerModal.autoSaveDialog;
+    await this.page.addLocatorHandler(
+      autoSaveDialog.dialog,
+      async () => {
+        await autoSaveDialog.dismiss();
+      },
+      { times: 1 }
+    );
 
     await this.designSurveyLink.click();
     await this.page.waitForLoadState('networkidle');
-
-    this.page.off('response', handleDesignerAlert);
   }
 }
