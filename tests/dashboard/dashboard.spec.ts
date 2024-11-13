c784d71f05ccd4f6ec0a298b19f66d1e59446a8c
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-11-13 10:31:06 -0600

    chore: stub out dashboard tests

---
+++ tests/dashboard/dashboard.spec.ts
@@ -0,0 +1,188 @@
+import { test as base, expect } from '../../fixtures';
+import { AnnotationType } from '../annotations';
+
+type DashboardTestFixtures = {};
+
+const test = base.extend<DashboardTestFixtures>({});
+
+test.describe('dashboard', () => {
+  test('Create a Dashboard via the create button in the header of the admin home page', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-316',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Create a Dashboard via the create button on the Dashboards tile on the admin home page', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-317',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Create a Dashboard via the "Create Dashboard" button on the Dashboards home page', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-318',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Create a copy of a Dashboard via the create button in the header of the admin home page', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-319',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Create a copy of a Dashboard via the create button on the Dashboards tile on the admin home page', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-320',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Create a copy of a Dashboard via the "Create Dashboard" button on the Dashboards home page', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-321',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Update a dashboard', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-322',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Delete a dashboard', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-323',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test("Edit a dashboard's configurations from the dashboard", () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-324',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Print a dashboard', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-325',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Export a dashboard', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-326',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Schedule a dashboard for export', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-327',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test("Get a dashboard's link", () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-328',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Set a dashboard as your default dashboard', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-329',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Disable a dashboard', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-330',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Enable a dashboard', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-331',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Make a dashboard private', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-332',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Make a dashboard public', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-333',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test("Navigate to a dashboard via it's link", () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-334',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Add a dashboard title to a dashboard', () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-869',
+    });
+
+    expect(true).toBeTruthy();
+  });
+});
