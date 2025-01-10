935fde3a2adf4d1c06a4f2a35e5bdbdb521cb394
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-01-10 16:06:59 -0600

    chore: set a maxDiffPixelRation

--- playwright.config.ts
+++ playwright.config.ts
@@ -59,6 +59,9 @@ export default defineConfig<PlaywrightTestConfig & ApiTestOptions>({
   timeout: 5 * MS_PER_MIN,
   expect: {
     timeout: expectNavAndActionTimeout,
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.01,
+    },
   },
   retries: isCI ? 2 : 0,
   fullyParallel: true,
