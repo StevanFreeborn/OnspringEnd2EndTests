e8da2acc5476203017f4862e88fc7bc327bc55b8
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-02-21 15:13:36 -0600

    tests: add test for Test-280

--- tests/systemReports/adminAuditHistoryReport.spec.ts
+++ tests/systemReports/adminAuditHistoryReport.spec.ts
@@ -1,14 +1,39 @@
-import { expect, test } from "../../fixtures";
+import { expect, test as base } from "../../fixtures";
+import { app } from "../../fixtures/app.fixtures";
+import { App } from "../../models/app";
+import { AdminAuditHistoryPage } from "../../pageObjectModels/systemReports/adminAuditHistoryPage";
 import { AnnotationType } from "../annotations";
 
+type AdminAuditHistoryTestFixtures = {
+  app: App;
+  adminAuditHistoryPage: AdminAuditHistoryPage;
+}
+
+const test = base.extend<AdminAuditHistoryTestFixtures>({
+  app: app,
+  adminAuditHistoryPage: async ({ sysAdminPage }, use) => await use(new AdminAuditHistoryPage(sysAdminPage)),
+});
+
 test.describe('admin audit history report', function() {
-  test('Filter the Audit History Report', async () => {
+  test('Filter the Audit History Report', async ({ adminAuditHistoryPage, sysAdminUser, app }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-280',
     });
 
-    expect(true).toBeTruthy();
+    await test.step('Navigate to the Audit History Report', async () => {
+      await adminAuditHistoryPage.goto();
+    });
+
+    await test.step('Filter the Audit History Report', async () => {
+      await adminAuditHistoryPage.applyFilter({ user: sysAdminUser.fullName, saveType: 'Added', itemType: 'App', appOrSurvey: app.name });
+    });
+
+    await test.step('Verify the Audit History Report has been filtered', async () => {
+      const rows = await adminAuditHistoryPage.getRows();
+
+      expect(rows.length).toBe(1);
+    });
   });
   
   test('Export the Audit History Report', async () => {
