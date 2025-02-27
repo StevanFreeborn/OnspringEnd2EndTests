214f8cc087e95601b176700b3691255c8c65e106
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-02-26 22:13:05 -0600

    tests: add test for Test-282

---
+++ pageObjectModels/systemReports/automatedMessageSourcesPage.ts
@@ -0,0 +1,108 @@
+import { Locator, Page } from "@playwright/test";
+import { BaseAdminPage } from "../baseAdminPage";
+
+type EmailType =
+  | "Data Connector"
+  | "Email Body"
+  | "Record Retention"
+  | "Scheduled Report"
+  | "Unlaunched Survey"
+  | "Workflow Finish"
+  | "Workflow Step";
+
+type AutomatedMessageSourceFilter = {
+  emailType?: EmailType;
+  appOrSurvey?: string | "All Apps & Surveys";
+  group?: string | "All Groups";
+  user?: string | "All Users";
+};
+
+export class AutomatedMessageSourcesPage extends BaseAdminPage {
+  private readonly path: string;
+  private readonly getAuditHistoryPath: string;
+  private readonly emailTypeSelector: Locator;
+  private readonly appOrSurveySelector: Locator;
+  private readonly groupSelector: Locator;
+  private readonly userSelector: Locator;
+  private readonly sourcesGridBody: Locator;
+  
+  constructor(page: Page) {
+    super(page);
+    this.path = '/Admin/Reporting/Messaging/AutomatedSources';
+    this.getAuditHistoryPath = '/Admin/Reporting/Messaging/GetAutomatedSourcesPage';
+    this.emailTypeSelector = this.page.locator('.label:has-text("Email Type") + .data').getByRole('listbox');
+    this.appOrSurveySelector = this.page.locator('.label:has-text("App/Survey") + .data').getByRole('listbox');
+    this.groupSelector = this.page.locator('.label:has-text("Group") + .data').getByRole('listbox');
+    this.userSelector = this.page.locator('.label:has-text("User") + .data').getByRole('listbox');
+    this.sourcesGridBody = this.page.locator('#grid .k-grid-content');  
+  }
+
+  async goto() {
+    const getAuditHistoryResponse = this.page.waitForResponse(this.getAuditHistoryPath);
+    await this.page.goto(this.path);
+    await getAuditHistoryResponse;
+  }
+
+  private async selectEmailType(emailType: string) {
+    await this.emailTypeSelector.click();
+    await this.page.getByRole('option', { name: emailType }).click();
+  }
+
+  private async selectAppOrSurvey(appOrSurvey: string) {
+    await this.appOrSurveySelector.click();
+    await this.page.getByRole('option', { name: appOrSurvey }).click();
+  }
+
+  private async selectGroup(group: string) {
+    await this.groupSelector.click();
+    await this.page.getByRole('option', { name: group }).click();
+  }
+
+  private async selectUser(user: string) {
+    await this.userSelector.click();
+    await this.page.getByRole('option', { name: user }).click();
+  }
+
+  async applyFilter({
+    emailType = "Email Body",
+    appOrSurvey = "All Apps & Surveys",
+    group = "All Groups",
+    user = "All Users",
+  }: AutomatedMessageSourceFilter) {
+    const existingEmailTypeFilter = await this.emailTypeSelector.innerText();
+
+    if (existingEmailTypeFilter.trim() !== emailType) {
+      const emailTypeFilterResponse = this.page.waitForResponse(this.getAuditHistoryPath);
+      await this.selectEmailType(emailType);
+      await emailTypeFilterResponse;
+    }
+
+    const existingAppOrSurveyFilter = await this.appOrSurveySelector.innerText();
+
+    if (existingAppOrSurveyFilter.trim() !== appOrSurvey) {
+      const appOrSurveyFilterResponse = this.page.waitForResponse(this.getAuditHistoryPath);
+      await this.selectAppOrSurvey(appOrSurvey);
+      await appOrSurveyFilterResponse;
+    }
+    
+    const existingGroupFilter = await this.groupSelector.innerText();
+
+    if (existingGroupFilter.trim() !== group) {
+      const groupFilterResponse = this.page.waitForResponse(this.getAuditHistoryPath);
+      await this.selectGroup(group);
+      await groupFilterResponse;
+    }
+
+    const existingUserFilter = await this.userSelector.innerText();
+
+    if (existingUserFilter.trim() !== user) {
+      const userFilterResponse = this.page.waitForResponse(this.getAuditHistoryPath);
+      await this.selectUser(user);
+      await userFilterResponse;
+    }
+  }
+
+  async getRows() {
+    return this.sourcesGridBody.locator('tr').all();
+  }
+}
--- tests/systemReports/automatedEmailMessageSourcesReport.spec.ts
+++ tests/systemReports/automatedEmailMessageSourcesReport.spec.ts
@@ -1,22 +1,75 @@
-import { expect, test } from '../../fixtures';
+import { FakeDataFactory } from '../../factories/fakeDataFactory';
+import { expect, test as base } from '../../fixtures';
+import { app } from '../../fixtures/app.fixtures';
+import { App } from '../../models/app';
+import { EmailBody } from '../../models/emailBody';
+import { TextRuleWithValue } from '../../models/rule';
+import { SimpleRuleLogic } from '../../models/ruleLogic';
+import { TextField } from '../../models/textField';
+import { AppAdminPage } from '../../pageObjectModels/apps/appAdminPage';
+import { EditEmailBodyPage } from '../../pageObjectModels/messaging/editEmailBodyPage';
+import { AutomatedMessageSourcesPage } from '../../pageObjectModels/systemReports/automatedMessageSourcesPage';
 import { AnnotationType } from '../annotations';
 
+type AutomatedEmailMessageSourcesReportTestFixtures = {
+  app: App;
+  appAdminPage: AppAdminPage;
+  editEmailBodyPage: EditEmailBodyPage;
+  automatedMessageSourcesPage: AutomatedMessageSourcesPage;
+};
+
+const test = base.extend<AutomatedEmailMessageSourcesReportTestFixtures>({
+  app: app,
+  appAdminPage: async ({ sysAdminPage }, use) => await use(new AppAdminPage(sysAdminPage)),
+  editEmailBodyPage: async ({ sysAdminPage }, use) => await use(new EditEmailBodyPage(sysAdminPage)),
+  automatedMessageSourcesPage: async ({ sysAdminPage }, use) => await use(new AutomatedMessageSourcesPage(sysAdminPage)),
+});
+
 test.describe('automated email message sources report', () => {
-  test('Filter the Automated Email Message Sources Report', async () => {
+  test('Filter the Automated Email Message Sources Report', async ({ app, appAdminPage, editEmailBodyPage, automatedMessageSourcesPage }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-282',
     });
+    
+    const emailBodyName = FakeDataFactory.createFakeEmailBodyName();
+    const textField = new TextField({ name: 'Send Email?'});
+    const rule = new TextRuleWithValue({
+      fieldName: textField.name,
+      operator: 'Contains',
+      value: 'Yes',
+    });
 
-    await test.step('Create an email body', async () => {});
+    const emailBody = new EmailBody({
+      name: emailBodyName,
+      appName: app.name,
+      status: true,
+      subject: `Test Subject - ${emailBodyName}`,
+      body: 'This is a test email body.',
+      fromName: 'Automation Test',
+      fromAddress: FakeDataFactory.createFakeEmailFromAddress(),
+      recipientsBasedOnFields: ['Created By'],
+      sendLogic: new SimpleRuleLogic({
+        rules: [rule],
+      }),
+    });
 
-    await test.step('Navigate to the automated email message sources report', async () => {});
+    await test.step('Create an email body', async () => {
+      await createEmailBody(appAdminPage, editEmailBodyPage, app, emailBody);
+    });
 
-    await test.step('Filter the automated email message sources report', async () => {});
+    await test.step('Navigate to the automated email message sources report', async () => {
+      await automatedMessageSourcesPage.goto();
+    });
 
-    await test.step('Verify the automated email message sources report is filtered', async () => {});
+    await test.step('Filter the automated email message sources report', async () => {
+      await automatedMessageSourcesPage.applyFilter({ emailType: 'Email Body', appOrSurvey: app.name });
+    });
 
-    expect(true).toBeTruthy();
+    await test.step('Verify the automated email message sources report is filtered', async () => {
+      const rows = await automatedMessageSourcesPage.getRows();
+      expect(rows.length).toBe(1);
+    });
   });
 
   test('Edit an item in the Automated Email Message Sources Report', async () => {
@@ -32,7 +85,7 @@ test.describe('automated email message sources report', () => {
     await test.step('Edit the email body from the automated email message sources report', async () => {});
 
     await test.step('Verify the email body was edited', async () => {});
-    
+
     expect(true).toBeTruthy();
   });
 
@@ -53,3 +106,15 @@ test.describe('automated email message sources report', () => {
     expect(true).toBeTruthy();
   });
 });
+
+async function createEmailBody(appAdminPage: AppAdminPage, editEmailBodyPage: EditEmailBodyPage, app: App, emailBody: EmailBody) {
+  await appAdminPage.goto(app.id);
+  await appAdminPage.layoutTabButton.click();
+  await appAdminPage.layoutTab.addLayoutItemFromFieldsAndObjectsGrid(new TextField({ name: 'Send Email?'}));
+
+  await appAdminPage.messagingTabButton.click();
+  await appAdminPage.messagingTab.createEmailBody(emailBody.name);
+  await appAdminPage.page.waitForURL(editEmailBodyPage.pathRegex);
+  await editEmailBodyPage.updateEmailBody(emailBody);
+  await editEmailBodyPage.save();
+}
