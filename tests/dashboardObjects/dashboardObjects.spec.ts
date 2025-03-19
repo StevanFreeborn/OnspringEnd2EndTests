95d93e0d2a7d93612c2c6ee28496ca1f9dc03911
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-03-19 11:15:10 -0500

    chore: stub out tests

---
+++ tests/dashboardObjects/dashboardObjects.spec.ts
@@ -0,0 +1,149 @@
+import { test, expect } from '../../fixtures';
+import { AnnotationType } from '../annotations';
+
+test.describe('dashboard objects', () => {
+  test('Add an App Search object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-758',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Add a Create Content Links object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-759',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Add a Formatted Text Block object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-760',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+
+  test('Add a Web Page object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-761',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Modify an App Search object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-762',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Modify a Create Content Links object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-763',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Modify a Formatted Text Block object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-764',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Modify a Web Page object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-765',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Delete an App Search object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-766',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Delete a Create Content Links object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-767',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Delete a Formatted Text Block object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-768',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Delete a Web Page object', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-769',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Verify an App Search object displays and functions as expected', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-770',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Verify a Create Content Links object displays and functions as expected', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-771',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Verify a Formatted Text Block object displays and functions as expected', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-772',
+    });
+
+    expect(true).toBeTruthy();
+  });
+
+  test('Verify a Web Page object displays and functions as expected', async () => {
+    test.info().annotations.push({
+      type: AnnotationType.TestId,
+      description: 'Test-773',
+    });
+
+    expect(true).toBeTruthy();
+  });
+});
