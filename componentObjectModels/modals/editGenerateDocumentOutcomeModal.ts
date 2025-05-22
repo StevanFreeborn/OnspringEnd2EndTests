57ae40a1a0ef976f807915ea5d867c795cb9fdf8
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-05-22 11:31:58 -0500

    feat: refactor document outcome modal and add creation settings tab functionality

--- componentObjectModels/modals/editGenerateDocumentOutcomeModal.ts
+++ componentObjectModels/modals/editGenerateDocumentOutcomeModal.ts
@@ -1,33 +1,23 @@
 import { Locator, Page } from '@playwright/test';
 import { GenerateDocumentOutcome } from '../../models/generateDocumentOutcome';
-import { EditOutcomeModal } from './editOutcomeModal';
+import { EditOutcomeModalWithTabs } from './editOutcomeModalWithTabs';
+import { GenerateDocumentCreationSettingsTab } from '../tabs/generateDocumentCreationSettingsTab';
 
-export class EditGenerateDocumentOutcomeModal extends EditOutcomeModal {
-  private readonly documentSelector: Locator;
-  private readonly fileTypeSelector: Locator;
-  private readonly attachmentFieldSelector: Locator;
+export class EditGenerateDocumentOutcomeModal extends EditOutcomeModalWithTabs {
+  private readonly creationSettingsTabButton: Locator;
+  private readonly creationSettingsTab: GenerateDocumentCreationSettingsTab;
 
   constructor(page: Page) {
     super(page);
-    this.documentSelector = this.modal.locator('.label:has-text("Document") + .data').getByRole('listbox');
-    this.fileTypeSelector = this.modal.locator('.label:has-text("File Type") + .data').getByRole('listbox');
-    this.attachmentFieldSelector = this.modal
-      .locator('.label:has-text("Attachment Field") + .data')
-      .getByRole('listbox');
+
+    this.creationSettingsTabButton = this.modal.getByRole('tab', { name: 'Creation Settings' });
+    this.creationSettingsTab = new GenerateDocumentCreationSettingsTab(this.modal);
   }
 
   async fillOutForm(outcome: GenerateDocumentOutcome) {
     await super.fillOutForm(outcome);
 
-    const page = this.modal.page();
-
-    await this.documentSelector.click();
-    await page.getByRole('option', { name: outcome.document }).click();
-
-    await this.fileTypeSelector.click();
-    await page.getByRole('option', { name: outcome.fileType }).click();
-
-    await this.attachmentFieldSelector.click();
-    await page.getByRole('option', { name: outcome.attachmentField }).click();
+    await this.creationSettingsTabButton.click();
+    await this.creationSettingsTab.fillOutForm(outcome);
   }
 }
