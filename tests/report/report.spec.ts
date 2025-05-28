b263d2e8117cca4dc64aab6229a6cd5d64c9023c
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-05-28 10:28:10 -0500

    chore: remove test.fail() assertion

--- tests/report/report.spec.ts
+++ tests/report/report.spec.ts
@@ -776,8 +776,6 @@ test.describe('report', () => {
 
     test.slow();
 
-    test.fail();
-
     await test.step('Create record in source app', async () => {
       await addContentPage.goto(sourceApp.id);
       await addContentPage.saveRecordButton.click();
