434d54c1b39fbc4a2c19b8b70e61654054aae709
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-07-28 11:50:42 -0500

    tests: add test for Test-423

---
+++ pageObjectModels/dataConnectors/editUcfConnectorPage.ts
@@ -0,0 +1,12 @@
+import { UcfConnectionTab } from '../../componentObjectModels/tabs/ucfConnectionTab';
+import { Page } from '../../fixtures';
+import { EditConnectorPage } from './editConnectorPage';
+
+export class EditUcfConnectorPage extends EditConnectorPage {
+  connectionTab: UcfConnectionTab;
+
+  constructor(page: Page) {
+    super(page);
+    this.connectionTab = new UcfConnectionTab(page);
+  }
+}
--- tests/dataConnector/ucf.spec.ts
+++ tests/dataConnector/ucf.spec.ts
@@ -1,18 +1,48 @@
+import { FakeDataFactory } from '../../factories/fakeDataFactory';
 import { test as base, expect } from '../../fixtures';
+import { DataConnectorAdminPage } from '../../pageObjectModels/dataConnectors/dataConnectorAdminPage';
+import { EditUcfConnectorPage } from '../../pageObjectModels/dataConnectors/editUcfConnectorPage';
 import { AnnotationType } from '../annotations';
 
-type UCFDataConnectorTestFixtures = {};
+type UCFDataConnectorTestFixtures = {
+  dataConnectorAdminPage: DataConnectorAdminPage;
+  editUcfConnectorPage: EditUcfConnectorPage;
+};
 
-const test = base.extend<UCFDataConnectorTestFixtures>({});
+const test = base.extend<UCFDataConnectorTestFixtures>({
+  dataConnectorAdminPage: async ({ sysAdminPage }, use) => await use(new DataConnectorAdminPage(sysAdminPage)),
+  editUcfConnectorPage: async ({ sysAdminPage }, use) => await use(new EditUcfConnectorPage(sysAdminPage)),
+});
 
 test.describe('ucf data connector', () => {
-  test('Create a new UCF connector', async () => {
+  let connectorsToDelete: string[] = [];
+
+  test.afterEach(async ({ dataConnectorAdminPage }) => {
+    await dataConnectorAdminPage.deleteConnectors(connectorsToDelete);
+    connectorsToDelete = [];
+  });
+
+  test('Create a new UCF connector', async ({ dataConnectorAdminPage, editUcfConnectorPage }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-423',
     });
 
-    expect(true).toBeTruthy();
+    const connectorName = FakeDataFactory.createFakeConnectorName();
+    connectorsToDelete.push(connectorName);
+
+    await test.step('Navigate to the data connectors admin page', async () => {
+      await dataConnectorAdminPage.goto();
+    });
+
+    await test.step('Create the UCF data connector', async () => {
+      await dataConnectorAdminPage.createConnector(connectorName, 'Unified Compliance Framework (UCF) Connector');
+      await dataConnectorAdminPage.page.waitForURL(editUcfConnectorPage.pathRegex);
+    });
+
+    await test.step('Verify the data connector was created successfully', async () => {
+      await expect(editUcfConnectorPage.connectionTab.nameInput).toHaveValue(connectorName);
+    });
   });
 
   test('Create a copy of a UCF connector', async () => {
