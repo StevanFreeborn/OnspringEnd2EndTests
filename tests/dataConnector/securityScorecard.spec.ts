2941de3723a3c7eb98654411adc89a2c914df02f
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-07-29 13:17:37 -0500

    tests: add test for Test-411

--- tests/dataConnector/securityScorecard.spec.ts
+++ tests/dataConnector/securityScorecard.spec.ts
@@ -89,12 +89,38 @@ test.describe('security scorecard data connector', () => {
     });
   });
 
-  test('Delete a Security Scorecard connector', async ({}) => {
+  test('Delete a Security Scorecard connector', async ({
+    dataConnectorAdminPage,
+    editSecurityScorecardConnectorPage,
+  }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-411',
     });
 
-    expect(true).toBeTruthy();
+    const connectorName = FakeDataFactory.createFakeConnectorName();
+
+    await test.step('Navigate to the data connectors admin page', async () => {
+      await dataConnectorAdminPage.goto();
+    });
+
+    await test.step('Create the data connector to delete', async () => {
+      await dataConnectorAdminPage.createConnector(connectorName, 'Security Scorecard Data Connector');
+      await dataConnectorAdminPage.page.waitForURL(editSecurityScorecardConnectorPage.pathRegex);
+    });
+
+    await test.step('Navigate back to the data connectors admin page', async () => {
+      await dataConnectorAdminPage.goto();
+    });
+
+    await test.step('Delete the data connector', async () => {
+      await dataConnectorAdminPage.deleteConnector(connectorName);
+    });
+
+    await test.step('Verify the data connector has been deleted', async () => {
+      const connectorRow = dataConnectorAdminPage.connectorsGrid.getByRole('row', { name: connectorName });
+
+      await expect(connectorRow).toBeHidden();
+    });
   });
 });
