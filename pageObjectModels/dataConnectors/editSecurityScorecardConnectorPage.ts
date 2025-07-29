f5d6f3bad8dff1891c84617d4a968821d630ee78
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-07-29 13:11:56 -0500

    tests: add test for Test-409

---
+++ pageObjectModels/dataConnectors/editSecurityScorecardConnectorPage.ts
@@ -0,0 +1,12 @@
+import { SecurityScorecardConnectionTab } from '../../componentObjectModels/tabs/securityScorecardConnectionTab';
+import { Page } from '../../fixtures';
+import { EditConnectorPage } from './editConnectorPage';
+
+export class EditSecurityScorecardConnectorPage extends EditConnectorPage {
+  readonly connectionTab: SecurityScorecardConnectionTab;
+
+  constructor(page: Page) {
+    super(page);
+    this.connectionTab = new SecurityScorecardConnectionTab(page);
+  }
+}
--- tests/dataConnector/securityScorecard.spec.ts
+++ tests/dataConnector/securityScorecard.spec.ts
@@ -1,18 +1,52 @@
+import { FakeDataFactory } from '../../factories/fakeDataFactory';
 import { test as base, expect } from '../../fixtures';
+import { DataConnectorAdminPage } from '../../pageObjectModels/dataConnectors/dataConnectorAdminPage';
+import { EditSecurityScorecardConnectorPage } from '../../pageObjectModels/dataConnectors/editSecurityScorecardConnectorPage';
 import { AnnotationType } from '../annotations';
 
-type SecurityScorecardDataConnectorTestFixtures = {};
+type SecurityScorecardDataConnectorTestFixtures = {
+  dataConnectorAdminPage: DataConnectorAdminPage;
+  editSecurityScorecardConnectorPage: EditSecurityScorecardConnectorPage;
+};
 
-const test = base.extend<SecurityScorecardDataConnectorTestFixtures>({});
+const test = base.extend<SecurityScorecardDataConnectorTestFixtures>({
+  dataConnectorAdminPage: async ({ sysAdminPage }, use) => await use(new DataConnectorAdminPage(sysAdminPage)),
+  editSecurityScorecardConnectorPage: async ({ sysAdminPage }, use) =>
+    await use(new EditSecurityScorecardConnectorPage(sysAdminPage)),
+});
 
 test.describe('security scorecard data connector', () => {
-  test('Create a new Security Scorecard connector', async ({}) => {
+  let connectorsToDelete: string[] = [];
+
+  test.afterEach(async ({ dataConnectorAdminPage }) => {
+    await dataConnectorAdminPage.deleteConnectors(connectorsToDelete);
+    connectorsToDelete = [];
+  });
+
+  test('Create a new Security Scorecard connector', async ({
+    dataConnectorAdminPage,
+    editSecurityScorecardConnectorPage,
+  }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-409',
     });
 
-    expect(true).toBeTruthy();
+    const connectorName = FakeDataFactory.createFakeConnectorName();
+    connectorsToDelete.push(connectorName);
+
+    await test.step('Navigate to the data connectors admin page', async () => {
+      await dataConnectorAdminPage.goto();
+    });
+
+    await test.step('Create the Security Scorecard data connector', async () => {
+      await dataConnectorAdminPage.createConnector(connectorName, 'Security Scorecard Data Connector');
+      await dataConnectorAdminPage.page.waitForURL(editSecurityScorecardConnectorPage.pathRegex);
+    });
+
+    await test.step('Verify the data connector was created successfully', async () => {
+      await expect(editSecurityScorecardConnectorPage.connectionTab.nameInput).toHaveValue(connectorName);
+    });
   });
 
   test('Create a copy of a Security Scorecard connector', async ({}) => {
