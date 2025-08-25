6e675414306027e5f0827d2f1e379aa0751fb189
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-08-25 11:49:26 -0500

    tests: add test for Test-360

---
+++ pageObjectModels/systemReports/emailMessageConfigReportPage.ts
@@ -0,0 +1,109 @@
+import { Locator, Page } from '../../fixtures';
+import { BaseAdminPage } from '../baseAdminPage';
+
+type EmailType =
+  | 'All Email Types'
+  | 'Email Body'
+  | 'Scheduled Report'
+  | 'Scheduled Dashboard'
+  | 'Survey Campaign'
+  | 'Workflow Approval'
+  | 'Workflow Rejection'
+  | 'Workflow Step';
+
+type FromAddressStatus = 'All' | 'OK' | 'Requires Attention';
+
+export class EmailMessageConfigReportPage extends BaseAdminPage {
+  private readonly getConfigsPath: string;
+  private readonly emailTypeSelector: Locator;
+  private readonly fromAddressStatusSelector: Locator;
+  private readonly appOrSurveySelector: Locator;
+  private readonly reportGrid: Locator;
+  private readonly reportGridBody: Locator;
+  readonly path: string;
+
+  constructor(page: Page) {
+    super(page);
+    this.getConfigsPath = '/Admin/Reporting/Messaging/GetConfigurationsPage';
+    this.emailTypeSelector = this.page.locator('.label:has-text("Email Type") + .data').getByRole('listbox');
+    this.fromAddressStatusSelector = this.page
+      .locator('.label:has-text("From Address Status") + .data')
+      .getByRole('listbox');
+    this.appOrSurveySelector = this.page.locator('.label:has-text("App/Survey") + .data').getByRole('listbox');
+    this.reportGrid = this.page.locator('#grid');
+    this.reportGridBody = this.reportGrid.locator('.k-grid-content');
+    this.path = '/Admin/Reporting/Messaging/Configurations';
+  }
+
+  async goto() {
+    const response = this.page.waitForResponse(this.getConfigsPath);
+    await this.page.goto(this.path);
+    await response;
+  }
+
+  private async filterConfigs(action: () => Promise<void>) {
+    const getLoginHistoryDataResponse = this.page.waitForResponse(this.getConfigsPath);
+    await action();
+    await getLoginHistoryDataResponse;
+  }
+
+  private async hasSameValue(value: string, locator: Locator) {
+    const currentValue = await locator.textContent();
+    return currentValue?.toLowerCase().includes(value.toLowerCase());
+  }
+
+  private async selectEmailType(emailType: string) {
+    if (await this.hasSameValue(emailType, this.emailTypeSelector)) {
+      return;
+    }
+
+    await this.filterConfigs(async () => {
+      await this.emailTypeSelector.click();
+      await this.page.getByRole('option', { name: emailType }).click();
+    });
+  }
+
+  private async selectFromAddressStatus(status: string) {
+    if (await this.hasSameValue(status, this.fromAddressStatusSelector)) {
+      return;
+    }
+
+    await this.filterConfigs(async () => {
+      await this.fromAddressStatusSelector.click();
+      await this.page.getByRole('option', { name: status }).click();
+    });
+  }
+
+  private async selectAppOrSurvey(appOrSurvey: string) {
+    if (await this.hasSameValue(appOrSurvey, this.appOrSurveySelector)) {
+      return;
+    }
+
+    await this.filterConfigs(async () => {
+      await this.appOrSurveySelector.click();
+      await this.page.getByRole('option', { name: appOrSurvey }).click();
+    });
+  }
+
+  async filterReport({
+    type,
+    status,
+    appOrSurvey,
+  }: {
+    type: EmailType;
+    status: FromAddressStatus;
+    appOrSurvey: string;
+  }) {
+    await this.selectEmailType(type);
+    await this.selectFromAddressStatus(status);
+    await this.selectAppOrSurvey(appOrSurvey);
+  }
+
+  async getRows() {
+    return this.reportGridBody.locator('tr').all();
+  }
+
+  async getRowByName(name: string | RegExp) {
+    return this.reportGridBody.getByRole('row', { name });
+  }
+}
--- tests/systemReports/emailMessageConfigReport.spec.ts
+++ tests/systemReports/emailMessageConfigReport.spec.ts
@@ -1,18 +1,79 @@
+import { FakeDataFactory } from '../../factories/fakeDataFactory';
 import { test as base, expect } from '../../fixtures';
+import { app } from '../../fixtures/app.fixtures';
+import { App } from '../../models/app';
+import { EmailBody } from '../../models/emailBody';
+import { AppAdminPage } from '../../pageObjectModels/apps/appAdminPage';
+import { EditEmailBodyPage } from '../../pageObjectModels/messaging/editEmailBodyPage';
+import { EmailMessageConfigReportPage } from '../../pageObjectModels/systemReports/emailMessageConfigReportPage';
 import { AnnotationType } from '../annotations';
 
-type EmailMessageConfigReportTestFixtures = {};
+type EmailMessageConfigReportTestFixtures = {
+  app: App;
+  appAdminPage: AppAdminPage;
+  editEmailBodyPage: EditEmailBodyPage;
+  emailMessageConfigReportPage: EmailMessageConfigReportPage;
+};
 
-const test = base.extend<EmailMessageConfigReportTestFixtures>({});
+const test = base.extend<EmailMessageConfigReportTestFixtures>({
+  app: app,
+  appAdminPage: async ({ sysAdminPage }, use) => await use(new AppAdminPage(sysAdminPage)),
+  editEmailBodyPage: async ({ sysAdminPage }, use) => await use(new EditEmailBodyPage(sysAdminPage)),
+  emailMessageConfigReportPage: async ({ sysAdminPage }, use) =>
+    await use(new EmailMessageConfigReportPage(sysAdminPage)),
+});
 
 test.describe('email message configurations report', () => {
-  test('Filter the email messaging configurations report', async () => {
+  test('Filter the email messaging configurations report', async ({
+    app,
+    appAdminPage,
+    editEmailBodyPage,
+    emailMessageConfigReportPage,
+  }) => {
     test.info().annotations.push({
       type: AnnotationType.TestId,
       description: 'Test-360',
     });
 
-    expect(true).toBeTruthy();
+    const emailBody = new EmailBody({
+      name: FakeDataFactory.createFakeEmailBodyName(),
+      appName: app.name,
+      status: true,
+      subject: 'Test Subject',
+      body: 'Test',
+      fromName: 'Automation Test',
+      fromAddress: FakeDataFactory.createFakeEmailFromAddress(),
+      recipientsBasedOnFields: ['Created By'],
+    });
+
+    await test.step('Create an email body to find with a filter', async () => {
+      await appAdminPage.goto(app.id);
+      await appAdminPage.messagingTabButton.click();
+      await appAdminPage.messagingTab.createEmailBody(emailBody.name);
+      await appAdminPage.page.waitForURL(editEmailBodyPage.pathRegex);
+      await editEmailBodyPage.updateEmailBody(emailBody);
+      await editEmailBodyPage.save();
+    });
+
+    await test.step('Navigate to the email messaging configurations report page', async () => {
+      await emailMessageConfigReportPage.goto();
+    });
+
+    await test.step('Filter the email messaging configurations report', async () => {
+      await emailMessageConfigReportPage.filterReport({
+        appOrSurvey: app.name,
+        type: 'Email Body',
+        status: 'OK',
+      });
+    });
+
+    await test.step('Verify the email messaging configurations report is filtered', async () => {
+      const rows = await emailMessageConfigReportPage.getRows();
+      const row = await emailMessageConfigReportPage.getRowByName(emailBody.name);
+
+      expect(rows.length).toBe(1);
+      await expect(row).toBeVisible();
+    });
   });
 
   test('Sort the email messaging configurations report', async () => {
